d35c44b6d5fcaafd29c3370e0be5165a3028ce11
Mark Vogelgesang <movogelgesang@gmail.com>
2021-10-23 14:59:02 -0400

    Added about page along with specific classes to support it. (#13)
    * Added about page along with specific classes to support it.
    * update font-size to match blog-posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,8 +14,12 @@ module.exports = {
         label: "Posts",
         path: "/posts",
       },
+      {
+        label: "About",
+        path: "/about",
+      },
     ],
-    siteUrl: "https://mvogelgesang.com"
+    siteUrl: "https://mvogelgesang.com",
   },
   plugins: [
     "gatsby-plugin-image",
@@ -36,17 +40,17 @@ module.exports = {
           cookie_expires: 0,
         },
       },
-    },  
+    },
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
           `Montserrat\:ital,200,400,400i,500,700,800,400`,
-          `source sans pro\:300,400,400i,700` // you can also specify font weights and styles
+          `source sans pro\:300,400,400i,700`, // you can also specify font weights and styles
         ],
-        display: 'swap'
-      }
-    },    
+        display: "swap",
+      },
+    },
     {
       resolve: "gatsby-plugin-manifest",
       options: {
@@ -82,11 +86,11 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-robots-txt',
+      resolve: "gatsby-plugin-robots-txt",
       options: {
-        sitemap: 'https://mvogelgesang.com/sitemap.xml',
-        policy: [{ userAgent: '*', allow: '/' }]
-      }
+        sitemap: "https://mvogelgesang.com/sitemap.xml",
+        policy: [{ userAgent: "*", allow: "/" }],
+      },
     },
     {
       resolve: `gatsby-transformer-remark`,
@@ -102,20 +106,20 @@ module.exports = {
             resolve: "gatsby-remark-embed-gist",
             options: {
               // Optional:
-    
+
               // the github handler whose gists are to be accessed
               username: "mvogelgesang",
-    
+
               // a flag indicating whether the github default gist css should be preloaded or not
               // use this if you want to load the default css asynchronously.
               // default: false
-              gistCssPreload: true
-    
+              gistCssPreload: true,
+
               // a string that represents the github default gist css url.
               // defaults: "https://github.githubassets.com/assets/gist-embed-b3b573358bfc66d89e1e95dbf8319c09.css"
               //gistCssUrlAddress: "<string>"
-            }
-          }
+            },
+          },
         ],
       },
     },
