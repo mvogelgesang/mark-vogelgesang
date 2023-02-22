fad325c4041514f06b64fc5463b54fc2791f9c5e
mvogelgesang <movogelgesang@gmail.com>
2023-02-21 22:05:18 -0500

    TE CLI Resume Activity. Tried implementing mermaid diagrams but opted for a png instead.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,18 +6,22 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
     site: "https://mvogelgesang.com",
-    integrations: [mdx(), sitemap()],
-    markdown: {
-        shikiConfig: {
-            // Choose from Shiki's built-in themes (or add your own)
-            // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-            theme: "dark-plus",
-            // Add custom languages
-            // Note: Shiki has countless langs built-in, including .astro!
-            // https://github.com/shikijs/shiki/blob/main/docs/languages.md
-            langs: [],
-            // Enable word wrap to prevent horizontal scrolling
-            wrap: true,
-        },
-    },
+    integrations: [
+        mdx({
+            extendDefaultPlugins: true,
+            syntaxHighlight: "shiki",
+            shikiConfig: {
+                // Choose from Shiki's built-in themes (or add your own)
+                // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+                theme: "dark-plus",
+                // Add custom languages
+                // Note: Shiki has countless langs built-in, including .astro!
+                // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+                langs: [],
+                // Enable word wrap to prevent horizontal scrolling
+                wrap: true,
+            },
+        }),
+        sitemap(),
+    ],
 });
