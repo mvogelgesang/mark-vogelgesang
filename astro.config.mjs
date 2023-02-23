0d067aa07d9a4fd4670d5d6e9061da9effdf402d
Mark Vogelgesang <movogelgesang@gmail.com>
2023-02-22 21:14:10 -0500

    Initial commit containing GA4 using Partytown as proxy (#39)
    * Initial commit containing GA4 using Partytown as proxy
    * updated ga4 id

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,26 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
-    site: "https://mvogelgesang.com",
-    integrations: [
-        mdx({
-            extendDefaultPlugins: true,
-            syntaxHighlight: "shiki",
-            shikiConfig: {
-                // Choose from Shiki's built-in themes (or add your own)
-                // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-                theme: "dark-plus",
-                // Add custom languages
-                // Note: Shiki has countless langs built-in, including .astro!
-                // https://github.com/shikijs/shiki/blob/main/docs/languages.md
-                langs: [],
-                // Enable word wrap to prevent horizontal scrolling
-                wrap: true,
-            },
-        }),
-        sitemap(),
-    ],
+  site: "https://mvogelgesang.com",
+  integrations: [mdx({
+    extendDefaultPlugins: true,
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: "dark-plus",
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true
+    }
+  }), sitemap(), partytown()]
 });
