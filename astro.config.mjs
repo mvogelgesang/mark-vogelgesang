a48658f5639d75d3e4ffa192d96bc87c3ca63615
Mark Vogelgesang <movogelgesang@gmail.com>
2023-08-24 21:27:29 -0400

    Economist 2023 (#52)
    * Economist blog post
    * Added compression, sorted tags within blog postsand on Tags page, updated format of Tags page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,16 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
 export default defineConfig({
-    site: "https://mvogelgesang.com",
-    integrations: [mdx({
-        extendDefaultPlugins: true,
-        syntaxHighlight: "prism",
-    }), sitemap(), partytown()]
+  site: "https://mvogelgesang.com",
+  integrations: [mdx({
+    extendDefaultPlugins: true,
+    syntaxHighlight: "prism"
+  }), sitemap(), partytown(), compress()]
 });
