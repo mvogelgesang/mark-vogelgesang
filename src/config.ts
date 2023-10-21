93002862632cb08938f3c221d92723718ba1d097
mvogelgesang <movogelgesang@gmail.com>
2023-10-21 14:23:15 -0400

    fixed theme toggle scripts that broke ui, switched to Material Icons

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,11 @@
+/* eslint-disable indent */
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Mark Vogelgesang';
-export const SITE_DESCRIPTION = 'Pursuing better solutions';
-export const NAVIGATION = [{
+export const SITE_TITLE = "Mark Vogelgesang";
+export const SITE_DESCRIPTION = "Pursuing better solutions";
+export const NAVIGATION = [
+  {
     label: "MV",
     path: "/",
   },
@@ -14,16 +16,15 @@ export const NAVIGATION = [{
   {
     label: "About",
     path: "/about",
-  }];
+  },
+];
 export const SOCIAL_MEDIA = [
-    {
-      name: "linkedin",
-      url: "https://www.linkedin.com/in/markovogelgesang/",
-      icon: "fa-brands fa-linkedin-in",
-    },
-    {
-      name: "github",
-      url: "https://github.com/mvogelgesang",
-      icon: "fa-brands fa-github-alt",
-    },
-  ];
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/markovogelgesang/",
+  },
+  {
+    name: "GitHub",
+    url: "https://github.com/mvogelgesang",
+  },
+];
