2c6b81ac0e1351691f9780da2f6421d89c2d25f9
mvogelgesang <movogelgesang@gmail.com>
2024-04-20 14:36:14 -0400

    Fixed overflow issues on mobile and adopted expressive-code to display code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,25 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import partytown from "@astrojs/partytown";
-
 // https://astro.build/config
 import compress from "astro-compress";
+import astroExpressiveCode from "astro-expressive-code";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://mvogelgesang.com",
   integrations: [
+    astroExpressiveCode({
+      // You can set configuration options here
+      themes: ["github-dark"],
+      styleOverrides: {
+        // You can also override styles
+        borderRadius: "0.5rem",
+      },
+      defaultProps: { wrap: false },
+    }),
     mdx({
       extendDefaultPlugins: true,
-      syntaxHighlight: "prism",
     }),
     sitemap(),
     partytown(),
