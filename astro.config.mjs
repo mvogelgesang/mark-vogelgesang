5812aec3be0e8485c611c5957d578b365d495473
mvogelgesang <movogelgesang@gmail.com>
2024-04-21 13:07:41 -0400

    Added tailwind as dependency, started config and update of PageWrapper

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,27 +2,24 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
-// https://astro.build/config
 import compress from "astro-compress";
 import astroExpressiveCode from "astro-expressive-code";
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://mvogelgesang.com",
-  integrations: [
-    astroExpressiveCode({
-      // You can set configuration options here
-      themes: ["github-dark"],
-      styleOverrides: {
-        // You can also override styles
-        borderRadius: "0.5rem",
-      },
-      defaultProps: { wrap: false },
-    }),
-    mdx({
-      extendDefaultPlugins: true,
-    }),
-    sitemap(),
-    partytown(),
-    compress(),
-  ],
+  integrations: [astroExpressiveCode({
+    // You can set configuration options here
+    themes: ["github-dark"],
+    styleOverrides: {
+      // You can also override styles
+      borderRadius: "0.5rem"
+    },
+    defaultProps: {
+      wrap: false
+    }
+  }), mdx({
+    extendDefaultPlugins: true
+  }), sitemap(), partytown(), compress(), tailwind()]
 });
