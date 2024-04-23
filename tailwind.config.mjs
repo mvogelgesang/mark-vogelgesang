bc945cfc82c88fa759651ee854e647dc6b51e512
mvogelgesang <movogelgesang@gmail.com>
2024-04-22 20:39:31 -0400

    Updated full header, migrated colors into tailwind config and using css custom properties

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
+/* eslint-disable indent */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        background: "hsl(var(--color-bg) / <alpha-value>)",
+        text: "hsl(var(--color-text) / <alpha-value>)",
+        "primary-color": "hsl(var(--color-primary) / <alpha-value>)",
+        "primary-200": "hsl(var(--color-primary-200) / <alpha-value>)",
+        "primary-400": "hsl(var(--color-primary-400) / <alpha-value>)",
+        "secondary-color": "hsl(var(--color-secondary) / <alpha-value>)",
+        "accent-one": "hsl(var(--color-accent-1) / <alpha-value>)",
+        "accent-two": "hsl(var(--color-accent-2) / <alpha-value>)",
+      },
       maxWidth: {
         128: "60rem",
       },
