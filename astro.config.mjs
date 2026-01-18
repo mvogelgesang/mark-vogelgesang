1846e1e8e0a55c07fafd5e65f7db5988928ef8e9
Mark Vogelgesang <movogelgesang@gmail.com>
2026-01-17 21:41:01 -0500

    Astro5 (#83)
    * upgrade to tailwind 4
    
    * upgrade to astro5
    
    * refreshed package-lock
    
    * feat: upgrade to astro v5 and fix build issues
    
    * resolved tailwind and postcss errors
    
    * resolving dependency conflicts

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,8 +27,5 @@ export default defineConfig({
     },
   }), mdx({
     extendDefaultPlugins: true,
-  }), tailwind()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  })],
 });
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,18 @@
 import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
 
-export const get = () =>
-	rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: import.meta.env.SITE,
-		items: import.meta.glob('./blog/**/*.{md,mdx}'),
-	});
+export async function get(context) {
+  const blog = await getCollection('blog');
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/blog/${post.slug}/`,
+    })),
+  });
+}
